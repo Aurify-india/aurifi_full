aaf220b88db5b7d6966e60b0db6ddce2a45a60a5
UjwalThakkar <ujwalpotoba02@gmail.com>
2025-07-04 15:05:43 +0530

    fixed proceed button navigation

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -227,7 +227,7 @@
 					header_mapping: '/DebtSheet/column_mapping',
 					datatype_conversion: '/DebtSheet/data_validation',
 					select_tags: '/DebtSheet/Tagging',
-					apply_rules: '/DebtSheet/retagged_items',
+					apply_rules: '/DebtSheet/Tagging',
 					completed: '/DebtSheet/file_overview'
 				};
 
